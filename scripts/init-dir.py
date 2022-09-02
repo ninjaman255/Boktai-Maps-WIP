b56d9e29e9b313ef3469c9b32cf36a107ca75ead
Kodedile <kodedile@gmail.com>
2022-09-01 21:13:56 -0400

    feat(dir): add script to turn data into directories

---
+++ scripts/init-dir.py
@@ -0,0 +1,123 @@
+# author: @kodedile
+# ---
+# Initialize folders for each of the dungeons 
+# and for each of the paths provided 
+# in the associated game's tsv files
+# ---
+
+import os
+import csv
+
+
+# init directory names
+
+dir_game = input("Which game directory? (default: '../Boktai 1') ")
+if dir_game == "":
+    dir_game = '../Boktai 1'
+
+
+# init tsv filenames
+tsv_dungeons = '../data/' + dir_game + '/dungeons.tsv';
+tsv_paths = '../data/' + dir_game + '/paths.tsv';
+
+
+# ask if we should separate folders into map areas
+separate = ""
+while not separate.strip():
+    separate = input("Group each folder by map area? [y/n] ")
+if separate.strip().lower() == "y":
+    separate = True
+else:
+    separate = False
+
+
+# init directories
+
+dungeons = '/Dungeons'
+paths = '/Paths'
+
+dir_dungeons = dir_game + dungeons
+dir_paths = dir_game + paths
+
+dir_dungeons_field = dir_dungeons
+dir_paths_field = dir_paths
+
+if not os.path.exists(dir_dungeons):
+    os.mkdir(dir_dungeons)
+
+if not os.path.exists(dir_paths):
+    os.mkdir(dir_paths)
+
+
+# init row counter
+count = 0
+
+# initialize folders for each dungeon
+with open(tsv_dungeons, 'r', encoding='utf8') as data:
+    data = csv.reader(data, delimiter='\t')
+    for row in data:
+        # skip first row
+        if count > 0:
+            name = row[0] + ' - ' + row[4]
+            
+            # setup field directory if requested
+            field = ""
+            if separate:
+                field = '/Field ' + row[2]
+                dir_dungeons_field = dir_game + field + dungeons
+                
+                if not os.path.exists(dir_game + field):
+                    os.mkdir(dir_game + field)
+                
+                if not os.path.exists(dir_dungeons_field):
+                    os.mkdir(dir_dungeons_field)
+            
+            # setup dungeon directory
+            if not os.path.exists(dir_dungeons_field + '/' + name):
+                os.mkdir(dir_dungeons_field + '/' + name)
+            
+            # gitkeep
+            outfile = dir_dungeons_field + '/' + name + '/.gitkeep'
+            print(outfile)
+            if not os.path.exists(outfile):
+                with open(outfile, 'w') as fp:
+                    pass
+            
+        count += 1
+
+
+# reset row counter
+count = 0
+
+# initialize folders for each pathway
+with open(tsv_paths, 'r', encoding='utf8') as data:
+    data = csv.reader(data, delimiter='\t')
+    for row in data:
+        # skip first row
+        if count > 0:
+            name = row[0] + ' - ' + row[2]
+            
+            # setup field directory if requested
+            field = ""
+            if separate:
+                field = '/Field ' + row[1]
+                dir_paths_field = dir_game + field + paths
+                
+                if not os.path.exists(dir_game + field):
+                    os.mkdir(dir_game + field)
+                
+                if not os.path.exists(dir_paths_field):
+                    os.mkdir(dir_paths_field)
+            
+            # setup pathway directory
+            if not os.path.exists(dir_paths_field + '/' + name):
+                os.mkdir(dir_paths_field + '/' + name)
+            
+            # gitkeep
+            outfile = dir_paths_field + '/' + name + '/.gitkeep'
+            print(outfile)
+            if not os.path.exists(outfile):
+                with open(outfile, 'w') as fp:
+                    pass
+            
+        count += 1
